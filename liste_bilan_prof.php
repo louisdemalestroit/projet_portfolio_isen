a038563ef58af7f5b4d853fe3a8c7d676d9e1ade
louisdemalestroit <louis.demalestroit2@gmail.com>
2025-04-28 10:47:57 +0200

    Update liste_bilan_prof.php

--- liste_bilan_prof.php
+++ liste_bilan_prof.php
@@ -2,11 +2,11 @@
 header("Content-Type: application/json");
 
 // Connexion à la base de données PostgreSQL
-$servername = "localhost";
-$username = "postgres";
-$password = "root";
-$dbname = "iddentite";
-
+$host = "dpg-d07jpbhr0fns738kroq0-a";  // Le host de ta base de données Render
+$port = "5432";  // Le port de PostgreSQL
+$dbname = "iddentite";  // Le nom de la base de données
+$user = "iddentite_user";  // L'utilisateur de la base de données
+$password = "dTgQCI7wlWV9JgkGqeUDJ6AdydeJA9JH";  // Le mot de passe de l'utilisateur
 try {
     $pdo = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
