e5e70a989835e7c33e3e48bc95f3749fd6452d24
louisdemalestroit <louis.demalestroit2@gmail.com>
2025-04-28 10:48:10 +0200

    Update notation_prof.php

--- notation_prof.php
+++ notation_prof.php
@@ -1,10 +1,11 @@
 <?php
 header("Content-Type: application/json");
 
-$host = 'localhost';
-$dbname = 'iddentite';
-$username = 'postgres';
-$password = 'root';
+$host = "dpg-d07jpbhr0fns738kroq0-a";  // Le host de ta base de données Render
+$port = "5432";  // Le port de PostgreSQL
+$dbname = "iddentite";  // Le nom de la base de données
+$user = "iddentite_user";  // L'utilisateur de la base de données
+$password = "dTgQCI7wlWV9JgkGqeUDJ6AdydeJA9JH";  // Le mot de passe de l'utilisateur
 
 try {
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
