6776b837e97a5bdbc13346a01e86082612c8c894
louisdemalestroit <louis.demalestroit2@gmail.com>
2025-04-28 10:47:23 +0200

    Update eleve.php

--- eleve.php
+++ eleve.php
@@ -1,8 +1,9 @@
 <?php
-$host = 'localhost';
-$db = 'iddentite';
-$user = 'postgres';
-$pass = 'root';
+$host = "dpg-d07jpbhr0fns738kroq0-a";  // Le host de ta base de données Render
+$port = "5432";  // Le port de PostgreSQL
+$dbname = "iddentite";  // Le nom de la base de données
+$user = "iddentite_user";  // L'utilisateur de la base de données
+$password = "dTgQCI7wlWV9JgkGqeUDJ6AdydeJA9JH";  // Le mot de passe de l'utilisateur
 
 try {
     $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
