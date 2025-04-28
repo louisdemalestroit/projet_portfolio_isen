d7b52d1baa4ef7570bf6c7593815327f8647f638
louisdemalestroit <louis.demalestroit2@gmail.com>
2025-04-28 10:38:43 +0200

    Update afficher_fichiers.php

--- afficher_fichiers.php
+++ afficher_fichiers.php
@@ -12,10 +12,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$host = "localhost"; 
-$dbname = "iddentite"; 
-$user = "postgres"; 
-$password = "root"; 
+$host = "dpg-d07jpbhr0fns738kroq0-a";  // Le host de ta base de données Render
+$port = "5432";  // Le port de PostgreSQL
+$dbname = "iddentite";  // Le nom de la base de données
+$user = "iddentite_user";  // L'utilisateur de la base de données
+$password = "dTgQCI7wlWV9JgkGqeUDJ6AdydeJA9JH";  // Le mot de passe de l'utilisateur
 
 try {
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password, [
