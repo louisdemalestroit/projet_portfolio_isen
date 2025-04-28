6db1a00dc5e9c81e082bd716fff857d0e2a07d59
louisdemalestroit <louis.demalestroit2@gmail.com>
2025-04-28 10:47:40 +0200

    Update getEleveByNomPrenom.php

--- getEleveByNomPrenom.php
+++ getEleveByNomPrenom.php
@@ -1,8 +1,9 @@
 <?php
-$host = 'localhost';
-$dbname = 'iddentite';
-$username = 'postgres';
-$password = 'root';
+$host = "dpg-d07jpbhr0fns738kroq0-a";  // Le host de ta base de données Render
+$port = "5432";  // Le port de PostgreSQL
+$dbname = "iddentite";  // Le nom de la base de données
+$user = "iddentite_user";  // L'utilisateur de la base de données
+$password = "dTgQCI7wlWV9JgkGqeUDJ6AdydeJA9JH";  // Le mot de passe de l'utilisateur
 
 // Vérifie que la requête est une requête GET
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
