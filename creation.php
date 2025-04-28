78b1f1a3ca76934e0b31901b82bc893a639b2b21
louisdemalestroit <louis.demalestroit2@gmail.com>
2025-04-28 10:42:32 +0200

    Update creation.php

--- creation.php
+++ creation.php
@@ -1,10 +1,11 @@
 <?php
 header("Content-Type: application/json");
 
-$host = "localhost";
-$dbname = "iddentite";
-$user = "postgres";
-$password = "root";
+$host = "dpg-d07jpbhr0fns738kroq0-a";  // Le host de ta base de données Render
+$port = "5432";  // Le port de PostgreSQL
+$dbname = "iddentite";  // Le nom de la base de données
+$user = "iddentite_user";  // L'utilisateur de la base de données
+$password = "dTgQCI7wlWV9JgkGqeUDJ6AdydeJA9JH";  // Le mot de passe de l'utilisateur
 
 $masterPassword = "chaos controle"; // Master password défini
 
